ee0d6eded90166aa2c0ac99ed853a3904534d16c
Vinicius Dev <101262773+ViniciusRFerreira@users.noreply.github.com>
2022-09-09 09:58:31 -0300

    Add files via upload

--- lista_tarefas.js
+++ lista_tarefas.js
@@ -25,8 +25,34 @@ frm.btSelecionar.addEventListener("click", () => {
         return
     }
 
+    let aux = -1; //para indicar linha selecionda
+
     //percorrer a lista de elementos h5 inseridos na pagina, as tarefas
     for (let i = 0; i < tarefas.length; i++){
-        
+        //se a tag é a selecionada - a class tarefa selecionada deve ser utilizada
+        if (tarefas[i].className == "tarefa-selecionada"){
+            tarefas[i].className = "tarefa-normal"
+            aux = i;
+            break;
+        }
+    }
+
+    //se a linha selecionada for a ultima
+    if (aux == tarefas.length - 1){
+        aux = -1;    
+    }
+
+    tarefas[aux + 1].className = "tarefa-selecionada"; //muda o estilo da proxima linha; 
+
+});
+
+
+frm.btRetirar.addEventListener("click", () => {
+    const tarefas = document.querySelectorAll("h5");
+
+    let aux = -1;
+
+    if(aux == -1){
+        alert("Selecione uma tarefa para removê-lá...")
     }
 });
